5d1f1b9af6096d975708a530a13cc55f0e1ff66d
mraf <masrafas@gmail.com>
2016-09-22 23:04:39 +0100

    deploy solution

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', function ($scope) {
+
+    $scope.dishes = "";
+    $scope.message = "";
+
+    $scope.Calc = function(){
+        var dish = $scope.dishes;
+        dish = dish.trim();
+        if(dish == ""){
+          $scope.message = "Please enter data first!";
+          
+        }else{
+          var dishplus = dish.split(",");
+          if(dishplus.length <=3){
+            $scope.message = "Enjoy!";     
+          }else{
+            $scope.message = "Too Much!";            
+          }
+        }
+    };
+
+  });
+
+})();
